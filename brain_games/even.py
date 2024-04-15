5c8a5c0b9ebd20b8958463d119e37e4247ed1462
Alexandra <a.teneneva@gmail.com>
2024-04-15 23:32:00 +0300

    step 5

---
+++ brain_games/even.py
@@ -0,0 +1,24 @@
+import random
+
+
+def even ():
+  index = 0  
+  print('Answer "yes" if the number is even, otherwise answer "no".')
+
+  while index < 3: 
+    number = random.randint(1, 100)    
+    print("Question: " + str(number))
+    answer = input("Your answer: ")     
+    if number % 2 == 0 and answer == "yes":
+      print("Correct!") 
+    elif number % 2 == 0 and answer != "yes":
+      return print(answer + " is wrong answer ;(. Correct answer was 'yes'. Let's try again")
+    else:
+      if number % 2 != 0 and answer == "no":
+        print("Correct!")
+      else:
+        return print(answer + " is wrong answer ;(. Correct answer was 'no'. Let's try again")
+    index+=1
+  print("Congratulation")
+
+even ()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+from brain_games import cli
+from brain_games import even
+
+
+def main ():
+    even.even()
+
+if __name__ == '__main__':
+    main()
+
