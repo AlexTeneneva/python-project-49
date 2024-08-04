63b491371e1f820b04d4a9fe01961f6b5ada1e3f
Alexandra <a.teneneva@gmail.com>
2024-08-04 15:12:18 +0300

    fixed prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,18 +2,20 @@ import random
 
 question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def Check_Prime(n):
-    return n % 2 == 0
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-
-def data ():
+def data():
     expression = random.randint(1, 100)
-    if Check_Prime(expression):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    correct_answer = 'yes' if Check_Prime(expression) else 'no'
     return expression, correct_answer
 
-#d = 2
-#    while n % d != 0:
- #       d += 1
- #   return d == n
+
+# if Check_Prime(expression):
+#    correct_answer = 'yes'
+#else:
+#    correct_answer = 'no'
