7e623524754e578efe674b5889bf2f0a6bb9ce59
Alexandra <a.teneneva@gmail.com>
2024-08-13 23:54:18 +0300

    fix linter 1308

--- brain_games/even.py
+++ brain_games/even.py
@@ -22,14 +22,14 @@ def even():
             print("Correct!")
         elif number % 2 == 0 and answer != "yes":
             return print(f"{answer} is wrong answer ;(."
-                f" Correct answer was 'yes'."
-                f" Let's try again, {name}!")
+                        f" Correct answer was 'yes'."
+                        f" Let's try again, {name}!")
         else:
             if number % 2 != 0 and answer == "no":
                 print("Correct!")
             else:
                 return print(f"{answer} is wrong answer ;(."
-                    f" Correct answer was 'yes'."
-                    f" Let's try again, {name}!")
+                            f" Correct answer was 'yes'."
+                            f" Let's try again, {name}!")
         index += 1
     print("Congratulations, " + name + "!")
