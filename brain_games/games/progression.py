397a914b3e3f5211bcf3de9047c338f8f0e58c75
Alexandra <a.teneneva@gmail.com>
2024-09-08 17:59:34 +0300

    corrections 08.09-2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,7 +10,8 @@ def get_question_and_answer():
     stop = start + step * length
     progression = list(range(start, stop, step))
     hide_symbol_index = random.randrange(length)
-    correct_answer, progression[hide_symbol_index] = progression[hide_symbol_index], '..'
+    correct_answer, progression[hide_symbol_index] = (
+        progression[hide_symbol_index], '..')
     expression = ' '.join([str(x) for x in progression])
 
     return expression, correct_answer
